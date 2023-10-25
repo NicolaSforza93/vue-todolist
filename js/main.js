fc96db7dbff7ef9526a94d33ae3847a91fa6bdd2
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-25 15:12:45 +0200

    added all files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log('Vue To Do List');
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+        console.log('OK');
+    }
+}).mount('#app')
